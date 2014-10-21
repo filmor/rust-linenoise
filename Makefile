50da5b8057ab84aa3555cf9833ccc956f373d548
Don Browne <dbrowne@groupon.com>
2014-10-21 18:27:04 +0100

    Add cargo file

--- Makefile
+++ Makefile
@@ -2,17 +2,16 @@ INSTALL = install
 PREFIX = /usr/local
 INSTALL_LIB = $(PREFIX)/lib
 
-all: linenoise.c linenoise.h
-	$(CC) -Wall -W -Os -fPIC -c linenoise.c -o linenoise.o
+all: src/linenoise.c src/linenoise.h
+	$(CC) -Wall -W -Os -fPIC -c src/linenoise.c -o linenoise.o
 	$(CC) -shared linenoise.o -o liblinenoise.so
-	rustc --crate-type=lib --emit=obj linenoise.rs -C link-args="-L ."
 
-install:
-	$(INSTALL) liblinenoise*.so $(INSTALL_LIB)
+#install:
+#	$(INSTALL) liblinenoise*.so $(INSTALL_LIB)
 
-uninstall:
-	rm -f $(INSTALL_LIB)/liblinenoise.so
-	rm -f $(INSTALL_LIB)/liblinenoise-rust.so
+#uninstall:
+#	rm -f $(INSTALL_LIB)/liblinenoise.so
+#	rm -f $(INSTALL_LIB)/liblinenoise-rust.so
 
 clean:
 	rm -f linenoise.o liblinenoise*.so
--- src/linenoise.rs
+++ src/linenoise.rs
@@ -1,5 +1,5 @@
 // Crate linkage metadata
-#![link(name = "linenoise", vers = "0.1", author = "davbo, donbrowne")]
+#![link(name = "linenoise", vers = "0.2", author = "davbo, donbrowne")]
 
 // Make a library ("bin" is the default)
 #![crate_type = "lib"]
@@ -18,40 +18,38 @@ struct linenoiseCompletions {
 extern {
     fn linenoise(prompt: *mut libc::c_char) -> *mut libc::c_char;
     fn linenoiseHistoryAdd(line: *mut libc::c_char) -> libc::c_int;
-    //fn linenoiseHistorySetMaxLen(length: libc::c_int) -> libc::c_int;
+    fn linenoiseHistorySetMaxLen(length: libc::c_int) -> libc::c_int;
     fn linenoiseHistorySave(filename: *mut libc::c_char) -> libc::c_int;
-    //fn linenoiseHistoryLoad(filename: *mut libc::c_char) -> libc::c_int;
+    fn linenoiseHistoryLoad(filename: *mut libc::c_char) -> libc::c_int;
     fn linenoiseSetMultiLine(enabled: libc::c_int) -> libc::c_void;
     fn linenoiseClearScreen() -> libc::c_void;
+    fn linenoisePrintKeyCodes() -> libc::c_void;
 }
 
 pub fn init(prompt: &str) -> String {
-    let line = prompt.with_c_str(|cstr| {
+    prompt.with_c_str(|cstr| {
         unsafe {
             let buf = linenoise(cstr as *mut i8);
             let line = CString::new(buf as *const i8, true);
             String::from_str(line.as_str().unwrap())
         }
-    });
-    line
+    })
 }
 
 pub fn history_add(line: &str) -> bool {
-    let added = line.with_c_str(|cstr| {
+    line.with_c_str(|cstr| {
         unsafe {
             linenoiseHistoryAdd(cstr as *mut i8)
         }
-    });
-    added as int == 1 // number of lines added perhaps?
+    }) as int == 1
 }
 
 pub fn history_save(filename: &str) -> bool {
-    let saved = filename.with_c_str(|cstr| {
+    filename.with_c_str(|cstr| {
         unsafe {
             linenoiseHistorySave(cstr as *mut i8)
         }
-    });
-    saved as int == 0 // Seems to always be 0 if successful?
+    }) as int == 0
 }
 
 pub fn set_multiline(enable: bool) {
